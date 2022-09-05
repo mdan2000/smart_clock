74d23c553bd67ec8508127bfe18c523a12c75751
mdan2000 <mdan_2000@mail.ru>
2022-09-05 03:03:26 +0300

    Added bad check of email

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,12 @@
 #include "FastLED.h"
 #include "TextForRun.h"
+#include "OutlookSetting.h"
 #include <WiFi.h>
+#include <ESP_Mail_Client.h>
+
+#define ENABLE_IMAP
+#define ESP_MAIL_USE_PSRAM
+
 
 // Указываем, какое количество пикселей у нашей ленты.
 #define LED_COUNT 512
@@ -9,8 +15,19 @@
 #define LED_PIN 33// Указываем, какое количество пикселей у нашей ленты.
 
 
-const char* ssid     = "AnyhipeTrain";
-const char* password = "TheBestWi-FiAtB212(2)";
+// Define the global used SMTP Session object which used for SMTP transsport
+SMTPSession smtp;
+
+// Define the global used session config data which used to store the TCP session configuration
+ESP_Mail_Session session;
+
+IMAPSession imap;
+IMAP_Config config;
+int checkEmailIterator = 1;
+
+
+const char* ssid     = "kek";
+const char* password = "ordasosatt";
 
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 7200;
@@ -18,7 +35,7 @@ const int   daylightOffset_sec = 3600;
 const int updateDelay = 80;
 
 bool matrixForBottomRow[8][500];
-String textForBottomRow = "        My Test String";
+String textForBottomRow = "       My Test String";
 int bottomRowWidth = 0;
 int bottomRowIteration = 0;
 
@@ -31,7 +48,7 @@ bool NEED_DISLAPY_BOTTOM_ROW = false;
 
 String makeTwoDigitsString(int i) {
   if (i < 10) {
-    return "0" + String(i); 
+    return "0" + String(i);
   } else {
     return String(i);
   }
@@ -40,7 +57,7 @@ String makeTwoDigitsString(int i) {
 void displayTime()
 {
   struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
+  if (!getLocalTime(&timeinfo)) {
     Serial.println("Failed to obtain time");
     return;
   }
@@ -53,11 +70,36 @@ void displayTime()
   displayMediumString(strip, res, 4, 9);
 }
 
-void setup() {
-  Serial.begin(9600);
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(strip, LED_COUNT);
-  FastLED.clear();
+void printPollingStatus(IMAPSession &imap)
+{
+  /* Declare the selected folder info class to get the info of selected mailbox folder */
+  SelectedFolderInfo sFolder = imap.selectedFolder();
 
+  /* Show the mailbox info */
+  ESP_MAIL_PRINTF("\nMailbox status changed\n----------------------\nTotal Messages: %d\n", sFolder.msgCount());
+
+  if (sFolder.pollingStatus().type == imap_polling_status_type_new_message)
+  {
+
+    ESP_MAIL_PRINTF("New message %d, has been addedd, reading message...\n", (int)sFolder.pollingStatus().messageNum);
+
+    // if (sFolder.recentCount() > 0)
+    //     ESP_MAIL_PRINTF("\nMesssage count which recent flag set: %d\n", sFolder.recentCount());
+
+    // we need to stop polling before do anything
+    imap.stopListen();
+
+    // Get the UID of new message and fetch
+    config.fetch.uid = imap.getUID(sFolder.pollingStatus().messageNum);
+    MailClient.readMail(&imap, false);
+  }
+  else if (sFolder.pollingStatus().type == imap_polling_status_type_remove_message)
+    ESP_MAIL_PRINTF("Message %d, has been removed\n\n", (int)sFolder.pollingStatus().messageNum);
+  else if (sFolder.pollingStatus().type == imap_polling_status_type_fetch_message)
+    ESP_MAIL_PRINTF("Message %d, has been fetched with the argument %s\n\n", (int)sFolder.pollingStatus().messageNum, sFolder.pollingStatus().argument.c_str());
+}
+
+void connectWiFi() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -70,8 +112,41 @@ void setup() {
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+}
 
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+void setup() {
+  Serial.begin(9600);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(strip, LED_COUNT);
+  FastLED.clear();
+
+  connectWiFi();
+
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+  Serial.println("Setting time");
+
+  session.server.host_name = "outlook.office365.com"; //for outlook.com
+  session.server.port = 993;
+  session.login.email = outlookEmail;
+  session.login.password = outlookPassword;
+
+  // Connect to the server with the defined session and options
+
+  // Open or select the mailbox folder to read the message
+  session.network_connection_handler = connectWiFi;
+
+  /* Connect to server with the session and config */
+  Serial.println("Try to connect to imap");
+  if (!imap.connect(&session, &config))
+    return;
+  Serial.println("Connecting to imap");
+
+  /*  {Optional} */
+  //printAllMailboxesInfo(imap);
+
+  /* Open or select the mailbox folder to read or search the message */
+  if (!imap.selectFolder(F("INBOX")))
+    Serial.println("No folder INBOX");
 }
 
 String getStringForBottomLine() {
@@ -81,6 +156,7 @@ String getStringForBottomLine() {
 int test = 0;
 
 void loop() {
+
   if (IS_DISPLAY_TIME) {
     displayTime();
   }
@@ -98,5 +174,20 @@ void loop() {
       NEED_DISLAPY_BOTTOM_ROW = false;
     }
   }
+
+  if (checkEmailIterator == 0) {
+    Serial.println("Checking email");
+    if (!imap.listen()) {
+      imap.listen();
+    }
+      // Check the changes
+      //if (imap.folderChanged())
+        printPollingStatus(imap);
+   // } else {
+   //   Serial.println("Can't listen imap");
+   // }
+  }
   delay(updateDelay);
+  checkEmailIterator++;
+  checkEmailIterator %= 24;
 }
